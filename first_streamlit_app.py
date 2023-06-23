a5c6a708ed5c1aa78428b855fa20ec61a7b24315
Thayana <mthayana16@gmail.com>
2023-06-23 15:05:45 +0530

    Update first_streamlit_app.py
    Corrected the Error

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title(" My Parents are heathy diner")
 streamlit.header("🥗 Breakfast Menu")
-streamlit.text(🥣 "Rice")
-streamlit.text(🥗 "Avocado Toast)
+streamlit.text("🥣 Rice")
+streamlit.text("🥗 Avocado Toast)
 streamlit.text("🐔 Omega Fish")
 streamlit.text("🥑kale,Spinach")
 streamlit.text("🍞french fries")
