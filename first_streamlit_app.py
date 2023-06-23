aafb85b70469420b5e595a394e319509a00d2354
Thayana <mthayana16@gmail.com>
2023-06-23 15:04:58 +0530

    Update first_streamlit_app.py
    Adding breakfast menu to the streamlitapp

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-streamlit.title("My Parents are heathy diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega Fish")
-streamlit.text("kale,Spinach")
-streamlit.text("french fries")
+streamlit.title(" My Parents are heathy diner")
+streamlit.header("🥗 Breakfast Menu")
+streamlit.text(🥣 "Rice")
+streamlit.text(🥗 "Avocado Toast)
+streamlit.text("🐔 Omega Fish")
+streamlit.text("🥑kale,Spinach")
+streamlit.text("🍞french fries")
