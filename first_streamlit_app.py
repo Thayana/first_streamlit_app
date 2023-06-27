f9338f9bcb92190c026e83ef33f680a2047b18c1
Thayana <mthayana16@gmail.com>
2023-06-28 00:00:27 +0530

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -33,7 +33,7 @@ try:
        streamlit.dataframe(back_from_function)
 except URLError as e:
        streamlit.error()
-streamlit.stop()
+#streamlit.stop()
 #import snowflake.connector
 
 streamlit.header("The fruit load list contains")
